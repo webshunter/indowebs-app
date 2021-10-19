6cd5aedd61b3c5ce1ac8746b1ce54cf374c731e0
webshunter <webshunternet@gmail.com>
2021-10-19 18:45:58 +0700

    baru

--- plugin/root.js
+++ plugin/root.js
@@ -1,6 +1,13 @@
 const rootP = {
     run: function() {
-        document.getElementById('app').innerHTML = 'update ke dua';
+        document.getElementById('app').innerHTML = `
+            <button id="exit-app">Exit App</button>
+        `;
+
+        document.getElementById('exit-app').addEventListener('click', function(){
+            ExitApp();
+        },false)
+
         return this;
     },
     runPage: function() {

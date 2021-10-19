ef4a618596bf08462a592142a49793e2b301439f
webshunter <webshunternet@gmail.com>
2021-10-19 19:24:54 +0700

    baru

--- plugin/root.js
+++ plugin/root.js
@@ -1,42 +1,10 @@
 const rootP = {
     run: function() {
-        document.getElementById('app').innerHTML = `
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <p>selamat datang di update live app for android with droid script</p>
-            <input type="text" class="form-control" placeholder="hold app" />
-            <button id="exit-app">Exit App 2</button>
-        `;
 
-        setTimeout(function() {
-            document.getElementById('exit-app').addEventListener('click', function(){
-                ExitApp();
-            },false)
-        },100)
+        var page = div().text('element app here');
 
 
+        document.getElementById('app').appendChild(page.get())
         return this;
     },
     runPage: function() {

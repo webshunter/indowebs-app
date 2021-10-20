f6c51ff5c0291fd341c390d3e20a69bbd22be542
webshunter <webshunternet@gmail.com>
2021-10-20 16:06:06 +0700

    new menu

--- plugin/root.js
+++ plugin/root.js
@@ -4,21 +4,35 @@ var page = div()
 .width('calc(100vw - 40px)')
 .padding('20px')
 
+
+page.child(
+    div().align('center')
+    .child(
+        span().text('CA Store')
+    )
+);
+
 // search of product
 
-.child(
+page.child(
     input().width('100%')
-    .padding('10px')
+    .padding('10px 20px')
     .css('border-radius', '20px')
     .css('outline', 'none')
     .marginTop('10px')
     .marginBottom('10px')
     .hold('ketik untuk mencari aksesoris atau baju.!!!')
-)
+);
+
+// style of menu
+
+var styleMenu = {
+    
+}
 
 // grid menu of app
 
-.child(
+page.child(
     div()
     .css('display', 'grid')
     .css('grid-template-columns', 'auto auto auto')
